a3a93385a8c3b93945000e90382eacc5440fc053
Vova_Reznik <reznikvova2222@gmail.com>
2021-02-07 16:10:21 +0200

    -avalidation commit

--- js/script.js
+++ js/script.js
@@ -158,14 +158,7 @@ document.addEventListener('keydown', (e) => {
 	}
 });
 
-function showModalByScroll() {
-	if ((window.pageYOffset + document.documentElement.clientHeight) >= document.documentElement.scrollHeight) {
-		openModal();
-		window.removeEventListener('scroll', showModalByScroll);
-	}
-}
 
-window.addEventListener('scroll', showModalByScroll);
 
 
 
